c8c1fa2d35dde8c501fd7c542ab898fd98a7db04
Fer <125735558+fernandosanchezz@users.noreply.github.com>
2025-04-11 11:32:33 +0200

    p4 v1

---
+++ P4_EDAT_GXXXX_PXX/P4_EDAT_G2103_P15/README.md
@@ -0,0 +1,29 @@
+# Reflexiones sobre el TAD BST
+
+*Al ejecutar el programa p4_e1.c se puede comprobar que los tiempos de creación y búsqueda varían mucho según si el programa se ejecuta en modo normal o en modo ordenado.*
+
+•¿Por qué es así?
+
+Cuando se ejecuta en modo ordenado los elementos entran previamente organizados esto provoca que  se aproveche mejor el espacio, rellenándose así los nodos de la derecha e izquierda, es por ello que podemos ver que su profundidad es considerablemente menor ya que se tratan de potencias de dos(50k ~ 2¹⁶ o 10k ~ 2¹⁴).
+
+•¿Hay alguna propiedad del árbol que permita explicar este comportamiento?
+
+Debido a que los árboles están jerarquizados, es decir que los elemntos están en distintos niveles, encontrar un elemento o donde insertarlo es más fácil si está previamente ordenado, porque como ya hemos dicho antes su profundidad es menor.
+
+# Reflexiones sobre el TAD SQ
+
+•¿Qué diferencias y similitudes hay entre el TAD SQ y el TAD Cola de la práctica anterior?
+
+En primer lugar, debido a que las SQ están formadas por un árbol están ordenadas a diferencia de las colas, además graias a esto funciones primitivas típcas de colas como push_front o push_back no tienen sentido, ya que cuando se inserta un elemento este se ordena, en segundo lugar, estas no tienen variables que apuntan al front o rear, porque estas pueden ser optenidas con funciones como find_min o find_max, por otro lado, el pop funciona también distinto al de una cola, ya que aunque si que extrae el primer elemento, como la SQ está ordenada no es siempre el primer elemento añadido, por último, las SQ a diferencia de las colas no pueden ser circulares.
+
+• ¿Qué coste (aproximado) tiene la operación de extraer un elemento en el TAD SQ? ¿Sería posible hacer que esta operación fuera 𝒪(1)?
+
+El coste vendrá determinado por la profundidad que tenga el arbol ya que se tiene que recorrer todo el subárbol izquierdo para encontrar el mínimo y luego hacer el proceso necesario para eliminar el elemento. Por lo que aproximadamente su coste sería de 𝒪(2n).
+En el caso de que el árbol solo tuviese un elemento, el coste sería 𝒪(1) ya que únicamente tendría que visitar este para saber que es el mínimo. En un caso general, no se podría, pero podríamos reducir el coste con un doble puntero al elemento mínimo, de esta forma eleminar el elemento sería de coste 𝒪(1), pero luego tendríamos que buscar el nuevo elemento mínimo, pasando a 𝒪(n).
+
+
+
+# Makefile
+
+Al hacer make solo se ejecutan las ordenes relacionadas con el ejercicio 1. 
+Pero si ejecutas make all y posteriormente make run_e1/e2 o make runv_e1/e2 (Para ejecutarlo con Valgrind) funciona perfectamente.
---
+++ P4_EDAT_GXXXX_PXX/P4_EDAT_G2103_P15/bstree.c
@@ -0,0 +1,354 @@
+#include "bstree.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* START [_BSTNode] */
+typedef struct _BSTNode {
+  void *info;
+  struct _BSTNode *left;
+  struct _BSTNode *right;
+} BSTNode;
+/* END [_BSTNode] */
+
+/* START [_BSTree] */
+struct _BSTree {
+  BSTNode *root;
+  P_ele_print print_ele;
+  P_ele_cmp cmp_ele;
+};
+/* END [_BSTree] */
+
+/*** BSTNode TAD private functions ***/
+BSTNode *_bst_node_new() {
+  BSTNode *pn = NULL;
+
+  pn = malloc(sizeof(BSTNode));
+  if (!pn) {
+    return NULL;
+  }
+
+  pn->left = NULL;
+  pn->right = NULL;
+  pn->info = NULL;
+
+  return pn;
+}
+
+void _bst_node_free(BSTNode *pn) {
+  if (!pn) {
+    return;
+  }
+
+  free(pn);
+}
+
+void _bst_node_free_rec(BSTNode *pn) {
+  if (!pn) {
+    return;
+  }
+
+  _bst_node_free_rec(pn->left);
+  _bst_node_free_rec(pn->right);
+  _bst_node_free(pn);
+
+  return;
+}
+
+int _bst_depth_rec(BSTNode *pn) {
+  int depth_l, depth_r;
+
+  if (!pn) {
+    return 0;
+  }
+
+  depth_l = _bst_depth_rec(pn->left);
+  depth_r = _bst_depth_rec(pn->right);
+
+  if (depth_r > depth_l) {
+    return depth_r + 1;
+  } else {
+    return depth_l + 1;
+  }
+}
+
+int _bst_size_rec(BSTNode *pn) {
+  int count = 0;
+
+  if (!pn) {
+    return count;
+  }
+
+  count += _bst_size_rec(pn->left);
+  count += _bst_size_rec(pn->right);
+
+  return count + 1;
+}
+
+int _bst_preOrder_rec(BSTNode *pn, FILE *pf, P_ele_print print_ele) {
+  int count = 0;
+
+  if (!pn) {
+    return count;
+  }
+
+  count += print_ele(pf, pn->info);
+  count += _bst_preOrder_rec(pn->left, pf, print_ele);
+  count += _bst_preOrder_rec(pn->right, pf, print_ele);
+
+  return count;
+}
+
+int _bst_inOrder_rec(BSTNode *pn, FILE *pf, P_ele_print print_ele) {
+  int count = 0;
+
+  if (!pn) {
+    return count;
+  }
+
+  if(pn->left!=NULL)
+    count += _bst_inOrder_rec(pn->left, pf, print_ele);
+  count += print_ele(pf, pn->info);
+  if(pn->right!=NULL)
+    count += _bst_inOrder_rec(pn->right, pf, print_ele);
+
+  return count;
+}
+
+int _bst_postOrder_rec(BSTNode *pn, FILE *pf, P_ele_print print_ele) {
+  int count = 0;
+
+  if (!pn) {
+    return count;
+  }
+
+  count += _bst_postOrder_rec(pn->left, pf, print_ele);
+  count += _bst_postOrder_rec(pn->right, pf, print_ele);
+  count += print_ele(pf, pn->info);
+
+  return count;
+}
+
+/*** BSTree TAD functions ***/
+BSTree *tree_init(P_ele_print print_ele, P_ele_cmp cmp_ele) {
+  BSTree *tree;
+
+  if (!print_ele || !cmp_ele) {
+    return NULL;
+  }
+
+  tree = malloc(sizeof(BSTree));
+  if (!tree) {
+    return NULL;
+  }
+
+  tree->root = NULL;
+  tree->print_ele = print_ele;
+  tree->cmp_ele = cmp_ele;
+
+  return tree;
+}
+
+void tree_destroy(BSTree *tree) {
+  if (!tree) {
+    return;
+  }
+
+  _bst_node_free_rec(tree->root);
+  free(tree);
+
+  return;
+}
+
+Bool tree_isEmpty(const BSTree *tree) {
+  if (!tree || !tree->root) {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+int tree_depth(const BSTree *tree) {
+  if (!tree) {
+    return -1;
+  }
+
+  return _bst_depth_rec(tree->root);
+}
+
+size_t tree_size(const BSTree *tree) {
+  if (!tree) {
+    return -1;
+  }
+
+  return _bst_size_rec(tree->root);
+}
+
+int tree_preOrder(FILE *f, const BSTree *tree) {
+  if (!f || !tree) {
+    return -1;
+  }
+
+  return _bst_preOrder_rec(tree->root, f, tree->print_ele) + fprintf(f, "\n");
+}
+
+int tree_inOrder(FILE *f, const BSTree *tree) {
+  if (!f || !tree) {
+    return -1;
+  }
+
+  return _bst_inOrder_rec(tree->root, f, tree->print_ele) + fprintf(f, "\n");
+}
+
+int tree_postOrder(FILE *f, const BSTree *tree) {
+  if (!f || !tree) {
+    return -1;
+  }
+
+  return _bst_postOrder_rec(tree->root, f, tree->print_ele) + fprintf(f, "\n");
+}
+
+/**** TODO: find_min, find_max, insert, contains, remove ****/
+
+BSTNode *_bst_find_min_rec(BSTNode *pn) {
+
+  if (!pn->left)
+    return pn;
+  else
+    return _bst_find_min_rec(pn->left);
+}
+
+void *tree_find_min(BSTree *tree) {
+  if (!tree)
+    return NULL;
+  return _bst_find_min_rec(tree->root)->info;
+}
+
+BSTNode *_bst_find_max_rec(BSTNode *pn) {
+
+  if (!pn->right)
+    return pn;
+  else
+    return _bst_find_max_rec(pn->right);
+}
+
+void *tree_find_max(BSTree *tree) {
+  if (!tree)
+    return NULL;
+  return _bst_find_max_rec(tree->root)->info;
+}
+
+Bool tree_contains(BSTree *tree, const void *elem) {
+  BSTNode *pn;
+  int cmp;
+  if (!tree || !elem)
+    return FALSE;
+
+  pn = tree->root;
+
+  if (!pn)
+    return FALSE;
+  do {
+    cmp = tree->cmp_ele(pn->info, elem);
+    if (cmp == 0)
+      return TRUE;
+    else if (cmp < 0) {
+      pn = pn->right;
+    } else
+      pn = pn->left;
+  } while (pn != NULL);
+
+  return FALSE;
+}
+
+BSTNode *_bst_insert_rec(BSTree *tree, BSTNode *node, const void *elem) {
+  int cmp;
+
+  if (!elem || !tree)
+    return NULL;
+
+  /*Se ha encontrado el hueco para insertar el nodo*/
+  if (node == NULL) {
+    if (!(node = _bst_node_new()))
+      return NULL;
+    node->info = (void *)elem;
+    return node;
+  }
+
+  cmp = tree->cmp_ele(elem, node->info);
+  if (cmp < 0)
+    node->left = _bst_insert_rec(tree, node->left, elem);
+  if (cmp > 0)
+    node->right = _bst_insert_rec(tree, node->right, elem);
+  return node;
+}
+
+Status tree_insert(BSTree *tree, const void *elem) {
+  BSTNode *ret;
+
+  if (!tree || !elem)
+    return ERROR;
+
+  ret = _bst_insert_rec(tree, tree->root, elem);
+
+  if (ret == NULL)
+    return ERROR;
+
+  tree->root = ret;
+
+  return OK;
+}
+
+BSTNode *_bst_remove_rec(BSTNode *pn, const void *elem, P_ele_cmp elem_cmp) {
+  int cmp;
+  BSTNode *ret;
+  if (!pn || !elem )
+    return NULL;
+
+  cmp = elem_cmp(elem, pn->info);
+
+  if (cmp < 0)
+    pn->left = _bst_remove_rec(pn->left, elem, elem_cmp);
+
+  else if (cmp > 0)
+    pn->right = _bst_remove_rec(pn->right, elem, elem_cmp);
+
+  else if (cmp == 0) {
+    if (!pn->right && !pn->left) {
+      _bst_node_free(pn);
+      return NULL;
+    }
+
+    else if (pn->right && !pn->left) {
+      ret = pn->right;
+      _bst_node_free(pn);
+      return ret;
+    }
+
+    else if (!pn->right && pn->left) {
+      ret = pn->left;
+      _bst_node_free(pn);
+      return ret;
+    }
+
+    else if (pn->right && pn->left) {
+      ret = _bst_find_min_rec(pn->right);
+      pn->info = ret->info;
+      pn->right = _bst_remove_rec(pn->right, ret->info, elem_cmp);
+      return pn;
+    }
+  }
+  return pn;
+}
+
+Status tree_remove(BSTree *tree, const void *elem) {
+  BSTNode *ret;
+
+  if (!tree || !elem)
+    return ERROR;
+
+  ret = _bst_remove_rec(tree->root, elem, tree->cmp_ele);
+
+  tree->root = ret;
+
+  return OK;
+}
